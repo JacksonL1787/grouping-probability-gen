8d1d946d5f1172da8f08130cfdefbf2f0fc9f2ef
Jackson Lawrence <jlawrence1787@gmail.com>
2020-03-12 14:19:11 -0700

    Changed Option

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const runSimLoop = 100;
-const runSimLoop2 = 10;
-const probN = 1.5;
-const probD = 10;
 const probabilities = [];
+const total = 18
+const math = 12
+const stats = 6
+const allGroups = [];
+
+// 18 students total
+// 12 Math
+// 6 stats
+// split into 2
+// one group got all Math
 
 const runSim = () => {
-
-  for(let i = 0; i < runSimLoop; i++) {
-    let tempProb = 0;
-    for(let a = 0; a < runSimLoop2; a++) {
-      let n = Math.random() * probD
-      if(n <= probN) {
-        tempProb++
+  let totalAllMath = 0;
+  for(var i = 0; i < runSimLoop; i++) {
+    const group1 = [];
+    const group2 = [];
+    for(var a = 0; a < total; a++) {
+      const n = Math.ceil(Math.random() * 18)
+      let student = n <= 6 ? "stats" : "math"
+      if(a % 2) {
+        group1.push(student)
+      } else {
+        group2.push(student)
       }
     }
-    probabilities.push(tempProb);
-    $('ul').append(`<li>${tempProb}/${probD}</li>`)
+    allGroups.push([group1, group2])
+    const allMath = ! group1.includes("stats") || ! group2.includes("stats")
+    $('ul').append(`<li><b>Group 1:</b>${group1} <b>Group 2:</b>${group2} - Only Math: ${allMath}</li>`)
+    if(allMath) totalAllMath++;
   }
-  console.log(probabilities)
-  const probSum = probabilities.reduce((a,b) => {
-    return a+b;
-  }, 0)
-  const probAvgExct = probSum/probabilities.length
-  const probAvgRnd = Math.round(probSum/probabilities.length)
-  $('.exact-prob').text(`Exact: ${probAvgExct}/${probD}`);
-  $('.rounded-prob').text(`Rounded: ${probAvgRnd}/${probD}`);
+  $('.prob').text(`All Math: ${totalAllMath}/${runSimLoop} or ${(totalAllMath/runSimLoop)*100}%`)
 }
 
 $(document).ready(() => {
